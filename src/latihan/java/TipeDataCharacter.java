ec6dc926cda30dde139d30e856a380cd7bb3068b
daffa dermawan <daffa181002@gmail.com>
2025-06-23 18:29:09 +0700

    lanjut ke tipe data character

--- src/latihan/java/TipeDataCharacter.java
+++ src/latihan/java/TipeDataCharacter.java
@@ -4,7 +4,13 @@ public class TipeDataCharacter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		char d = 'd';
+		char a = 'a';
+		char f = 'f';
+		
+		System.out.print(d);
+		System.out.print(a);
+		System.out.print(f);
 	}
 
 }
